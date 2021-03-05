2a8be2ca08616a272b0cf310b826c1eb8fb1f00c
Logan Zehr <zehrl315@outlook.com>
2021-03-04 16:40:14 -0800

    Pseudo code for index.js created. Created burger partial

---
+++ public/assets/js/index.js
@@ -0,0 +1,64 @@
+$(function() {
+    // // Initialize
+    // const initialize = () => {
+    //     // render "Ready to Eat Burgers" list items
+    //     // render "Consumed Burgers" list items
+
+    // }
+
+    // Submit burger
+        // handle inserting data into burgers_db
+        // clear entry box
+
+    // Consume burger
+        // handle deleting entry in burgers_db
+        // render "Ready to Eat Burgers" list items
+
+    // Reset consumed burgers
+        // handle deleting all entries if burgers are consumed
+        // render "Consumed Burgers" list items
+
+});
+
+// $(".change-sleep").on("click", function(event) {
+//   var id = $(this).data("id");
+//   var newSleep = $(this).data("newsleep");
+
+//   var newSleepState = {
+//     sleepy: newSleep
+//   };
+
+//   // Send the PUT request.
+//   $.ajax("/api/cats/" + id, {
+//     type: "PUT",
+//     data: newSleepState
+//   }).then(
+//     function() {
+//       console.log("changed sleep to", newSleep);
+//       // Reload the page to get the updated list
+//       location.reload();
+//     }
+//   );
+// });
+
+// $(".create-form").on("submit", function(event) {
+//   // Make sure to preventDefault on a submit event.
+//   event.preventDefault();
+
+//   var newCat = {
+//     name: $("#ca").val().trim(),
+//     sleepy: $("[name=sleepy]:checked").val().trim()
+//   };
+
+//   // Send the POST request.
+//   $.ajax("/api/cats", {
+//     type: "POST",
+//     data: newCat
+//   }).then(
+//     function() {
+//       console.log("created new cat");
+//       // Reload the page to get the updated list
+//       location.reload();
+//     }
+//   );
+// });
---
+++ views/partials/burger.js
@@ -0,0 +1,5 @@
+<ul class="list-group list-group-flush">
+    <li class="list-group-item d-flex justify-content-between align-items-center">1. Krabby Patty
+        <button type="button" class="btn btn-primary">🍔 Consume!</button>
+    </li>
+</ul>
