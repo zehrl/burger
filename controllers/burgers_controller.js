bb6ef46624cca20d6bce66313ccda1ec82563fdb
Logan Zehr <zehrl315@outlook.com>
2021-03-04 17:04:02 -0800

    burgers ready to eat now load using handlebars and database

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
 const burger = require('../models/burger');
 
 router.get('/', (req, res) => {
     burger.selectAll((data) => {
-        console.log(data);
-        res.render('index');
+        
+        const hbsObject = {
+            burger: data
+        };
+
+        console.log(hbsObject);
+        res.render('index', hbsObject);
     })
 });
 
