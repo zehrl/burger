10423e91080af4789a4300d1c93cb577b28c714c
Logan Zehr <zehrl315@outlook.com>
2021-03-05 13:41:49 -0800

    Took out consumedCount for now

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,7 @@ CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(255) NOT NULL,
     devoured BOOLEAN DEFAULT false,
+    display BOOLEAN DEFAULT true,
 
     PRIMARY KEY (id)
 );
